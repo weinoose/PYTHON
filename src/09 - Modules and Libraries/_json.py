9faa944932ca1e25630b66a1cdcd2bf6ca642469
Emir Yarkın Yaman <65957512+Weinoose@users.noreply.github.com>
2021-04-11 15:04:13 +0300

    Update _json.py

--- src/09 - Modules and Libraries/_json.py
+++ src/09 - Modules and Libraries/_json.py
@@ -1,6 +1,6 @@
 import json
 
-"""# DICTIONARY TO JSON
+# DICTIONARY TO JSON
 
 example1 = {
     "Name" : "Emir Yarkın",
@@ -10,13 +10,13 @@ example1 = {
 result = json.dumps(example1)
 
 with open("pythoncourse.json", "a", encoding="UTF-8") as file:
-    file.write(result)"""
+    file.write(result)
 
 # JSON TO DICTIONARY
 
-"""file = open("pythoncourse.json", "r")
+file = open("pythoncourse.json", "r")
 
 result = json.loads(file.read())
 print(result)
 
-file.close()"""
+file.close()
