6d0ea2793a0a47fc2c5f8f1789f1248e810239df
Emir Yarkın Yaman <65957512+Weinoose@users.noreply.github.com>
2021-06-15 00:45:46 +0300

    Update svc.py

--- src/18 - Machine Learning/Classification/svc.py
+++ src/18 - Machine Learning/Classification/svc.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 sc = StandardScaler()
 svc = SVC(kernel="linear")
