241010bfc77aa02257f1b4d65bc76d77634c4454
Emir Yarkın Yaman <65957512+WEINOOSE@users.noreply.github.com>
2021-11-14 17:04:57 +0300

    Delete SRC directory

--- SRC/07 - File Moderating/with.py
+++
@@ -1,9 +0,0 @@
-# APPEND
-
-with open("demo1.txt", "a", encoding="UTF-8") as file:
-    file.write("\nJavaScript\nR\nRuby\nPHP\nAngular")
-
-# READ
-
-with open("demo1.txt", "r", encoding="UTF-8") as file:
-    print(file.read())
--- SRC/09 - Modules and Libraries/mymodule.py
+++
@@ -1,8 +0,0 @@
-import WEINOOSE as weinoose
-
-result = weinoose.toplama(2,5)
-result = weinoose.cikarma(2,5)
-result = weinoose.bolme(2,5)
-result = weinoose.carpma(2,5)
-
-print(result)
